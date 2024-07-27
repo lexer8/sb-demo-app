8b74dca0276d395e1d77cbda0482fa85c942c743
Alex Lexer <Lexer8@mail.ru>
2024-07-27 17:20:56 +0200

    feat: content-type response header added

--- src/main/java/com/example/demo/controllers/OpsController.java
+++ src/main/java/com/example/demo/controllers/OpsController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/ops")
+@RequestMapping(value = "/ops", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OpsController {
 
     @GetMapping("/test")
